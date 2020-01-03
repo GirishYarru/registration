2fb2dbf2e55a3b7df5b7743f8bd9e665b392af35
M1048219 <girish.yarru@mindtree.com>
2020-01-03 10:25:04 +0530

    MOS-30705: Uin generator stage - Mapping json changes

---
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/constant/MappingJsonConstants.java
@@ -0,0 +1,10 @@
+package io.mosip.registration.processor.core.constant;
+
+public class MappingJsonConstants {
+	public static final String VALUE = "value";
+	public static final String POA = "poa";
+	public static final String POB = "pob";
+	public static final String POR = "por";
+	public static final String POI = "poi";
+	public static final String INDIVIDUAL_BIOMETRICS = "individualBiometrics";
+}
